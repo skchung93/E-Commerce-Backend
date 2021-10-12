34955879844288a665884ac33f064e3d8789294a
Skchung93 <skchung93@gmail.com>
2021-10-11 20:55:45 -0700

    Finally think I finished this project and got everything to work

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# E-Commerce-Backend
+# **Notepad App**
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+### Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributors](#contribution)
+- [Test](#test)
+- [License](#license)
+- [Email & Github](#contact)
+
+
+### DESCRIPTION <a name="description"></a>
+This is a project that was aimed at replicating the backend for an e-commerce site. 
+
+### Installation Instructions <a name="introduction"></a>
+Download or clone the repository from Github. Install the dependencies (using npm -i or node install). Start the application by running it in the terminal (npm start). After that, open insomnia and you'll be able to test the routes.
+
+### Usage Guide <a name="usage"></a>
+Use this code to deploy a sample version of the backend of an ecommerce site.
+
+### How to Contribute <a name="contribution"></a>
+If you'd like to contribute, please message me on Github.
+
+### How to Test the Application <a name="test"></a>
+If you want to test it, you'll need to create your own tests :)
+
+### Licenses <a name="license"></a>
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+(https://opensource.org/licenses/MIT)
+
+#### Email & Github <a name="contact"></a>
+skchung93@gmail.com </br>
+skchung93  
+
+
+
+
---
+++ routes/api/category-routes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [{model:Product}]
+    });
+    res.status(200).json(allCategories);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try{
+    const oneCategory = await Category.findByPk(req.params.id, {
+      include: [{model:Product}], 
+    });
+    res.status(200).json(oneCategory);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  }catch(err){
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const catData = await Category.update(
+    { 
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  return res.status(200).json(catData);
+  }catch(err){
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try{ 
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.json(catData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});    
+
+module.exports = router;
